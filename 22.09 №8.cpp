55987bb9da7c263514863335913f0443e2248d53
Begasova <Begasova@users.noreply.github.com>
2016-10-23 21:42:28 +0300

    Add files via upload

---
+++ 22.09 №8.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "Enter a, but 0 < a < 100 " << endl << "a = ";
+	cin >> a;
+	if (a > 100)
+	{
+		cout << "Input Error!" << endl;
+		system("pause");
+		return 0;
+	}
+	if (a > 9 && a < 20)
+	{
+		switch (a)
+		{
+		case 10:
+			cout << "ten " << endl;
+			break;
+		case 11:
+			cout << "eleven " << endl;
+			break;
+		case 12:
+			cout << "twelve " << endl;
+			break;
+		case 13:
+			cout << "thirteen " << endl;
+			break;
+		case 14:
+			cout << "fourteen " << endl;
+			break;
+		case 15:
+			cout << "fifteen " << endl;
+			break;
+		case 16:
+			cout << "sixteen " << endl;
+			break;
+		case 17:
+			cout << "seventeen " << endl;
+			break;
+		case 18:
+			cout << "eighteen " << endl;
+			break;
+		case 19:
+			cout << "nineteen " << endl;
+			break;
+		}
+	}
+	else
+	{
+		b = a / 10;
+		if (b == 0 || b >= 2 && b < 10)
+		{
+			switch (b)
+			{
+			case 0:
+				break;
+			case 2:
+				cout << "twenty " << endl;
+				break;
+			case 3:
+				cout << "thirty " << endl;
+				break;
+			case 4:
+				cout << "forty " << endl;
+				break;
+			case 5:
+				cout << "fifty " << endl;
+				break;
+			case 6:
+				cout << "sixty " << endl;
+				break;
+			case 7:
+				cout << "seventy " << endl;
+				break;
+			case 8:
+				cout << "eighty " << endl;
+				break;
+			case 9:
+				cout << "ninety " << endl;
+				break;
+			}
+		}
+
+		c = a % 10;
+		if (c >= 0)
+		{
+			switch (c)
+			{
+			case 0:
+				cout << "zero " << endl;
+				break;
+			case 1:
+				cout << "one " << endl;
+				break;
+			case 2:
+				cout << "two " << endl;
+				break;
+			case 3:
+				cout << "three " << endl;
+				break;
+			case 4:
+				cout << "four " << endl;
+				break;
+			case 5:
+				cout << "five " << endl;
+				break;
+			case 6:
+				cout << "six " << endl;
+				break;
+			case 7:
+				cout << "seven " << endl;
+				break;
+			case 8:
+				cout << "eight " << endl;
+				break;
+			case 9:
+				cout << "nine " << endl;
+				break;
+			}
+		}
+	}
+	system("pause");
+	return 1;
+}
